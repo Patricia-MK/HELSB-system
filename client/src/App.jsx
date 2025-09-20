aa35955e1d7f91c47c7c46c862d0907056f4f7ef
Chaabilo22 <lubobyachaabilo@gmail.com>
2025-09-20 22:15:22 +0200

    My local changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
+// src/App.jsx
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import ContactUs from "./components/ContactUs";
+import StudentDashboard from "./pages/StudentDashboard";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/contact" element={<ContactUs />} />
+      <Route path="/dashboard" element={<StudentDashboard />} />
       <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
